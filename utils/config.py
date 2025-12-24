2f01f10c4aafd56badbfc9c9db4481c091893c5c
hmdmdbe-stack <hmdmdbe@gmail.com>
2025-12-24 19:38:03 +0300

    Update bot_token to a hardcoded value
    Replaced bot_token environment variable with a hardcoded value.

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ env.read_env("./.env")
 
 api_id = env.int("API_ID")
 api_hash = env.str("API_HASH")
-bot_token = env.str("BOT_TOKEN")
+bot_token = "7005270762:AAGs-AJY9iIKiqK6tgO6WuKRhadpPmL_Odc"
 
 db_type = env.str("DATABASE_TYPE")
 db_url = env.str("DATABASE_URL", "")
